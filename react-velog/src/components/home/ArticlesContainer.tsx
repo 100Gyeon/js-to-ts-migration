4c6d643ddbdb2cfe31b9596ee73628d7ab9b5e4b
100Gyeon <bjy9966@naver.com>
2021-11-23 21:46:23 +0900

    chore: react-velog 초기 세팅

---
+++ react-velog/src/components/home/ArticlesContainer.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { client } from 'libs/api';
+import ArticleCard from './ArticleCard';
+
+const ArticlesContainer = () => {
+  const [articleData, setArticleData] = useState([]);
+
+  const getArticleData = async () => {
+    const { data } = await client.get('/article');
+    setArticleData(data);
+  };
+
+  useEffect(() => {
+    getArticleData();
+  }, []);
+
+  return (
+    <div>
+      {articleData.map((article) => (
+        <ArticleCard key={article.id} article={article} />
+      ))}
+    </div>
+  );
+};
+
+export default ArticlesContainer;
